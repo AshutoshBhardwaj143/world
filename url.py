80d6363029c49cb14747a19b9cce822a5947ee81
AshutoshBhardwaj143 <60725899+AshutoshBhardwaj143@users.noreply.github.com>
2020-02-27 09:53:47 +0530

    Add files via upload

---
+++ url.py
@@ -0,0 +1,6 @@
+import urllib.request
+webUrl=urllib.request. urlopen('https://wordpress.org/plugins/about/readme.txt')
+print("result code: "+str(webUrl.getcode()))
+
+data = webUrl.read()
+print (data)
